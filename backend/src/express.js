0443498c2f61d529b87d578c5fe29e1711427beb
Danny Fung <dannyfung97@gmail.com>
2019-04-28 17:53:51 -0700

    Added frontend and backend folders for organization, CORS issue exists

---
+++ backend/src/express.js
@@ -0,0 +1,163 @@
+
+const express = require('express')
+const app = express()
+const port = 4000
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+let Fabric_CA_Client = require('fabric-ca-client');
+
+let path = require('path');
+const { FileSystemWallet, Gateway, X509WalletMixin } = require('fabric-network');
+const fs = require('fs');
+let store_path = path.join(__dirname, 'hfc-key-store');
+console.log(' Store path:' + store_path);
+const ccpPath = path.resolve(__dirname, '..', 'network', 'connection.json');
+const ccpJSON = fs.readFileSync(ccpPath, 'utf8');
+const ccp = JSON.parse(ccpJSON);
+const caURL = ccp.certificateAuthorities['ca.example.com'].url;
+const ca = new Fabric_CA_Client(caURL);
+// Create a new file system based wallet for managing identities.
+const walletPath = path.join(process.cwd(), 'wallet');
+const wallet = new FileSystemWallet(walletPath);
+console.log(`Wallet path: ${walletPath}`);
+const gateway = new Gateway();
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Credentials', true);
+    next();
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+// const server = require('./server.js');
+
+var enrollAdmin = async function (id, pw) {
+    // Initialize Wallet Path
+    // Setup any other variable / directory structure that we need
+    // Enroll the Admin User
+    try {
+        //const wallet = new FileSystemWallet(walletPath);
+        // Check to see if we've already enrolled the admin user.
+        const adminExists = await wallet.exists('admin');
+        if (adminExists) {
+            console.log('An identity for the admin user "', id, '" already exists in the wallet');
+            return false;
+        }
+
+        // Enroll the admin user, and import the new identity into the wallet.
+        const enrollment = await ca.enroll({ enrollmentID: id, enrollmentSecret: pw });
+        const identity = X509WalletMixin.createIdentity('Org1MSP', enrollment.certificate, enrollment.key.toBytes());
+        wallet.import(id, identity);
+        console.log('Successfully enrolled admin user "', id, '" and imported it into the wallet');
+        return true;
+    } catch (error) {
+        console.error(`Failed to enroll admin user "${id}": ${error}`);
+        process.exit(1);
+    }
+}
+
+var register = async function (username, password) {
+    try {
+
+        const userExists = await wallet.exists(username);
+        if (userExists) {
+            console.log('An identity for the user "' + username + '" already exists in the wallet');
+            return false;
+        }
+
+        // Check to see if we've already enrolled the admin user.
+        const adminExists = await wallet.exists('admin');
+        if (!adminExists) {
+            console.log('An identity for the admin user "admin" does not exist in the wallet');
+            console.log('Run the enrollAdmin.js application before retrying');
+            return false;
+        }
+
+        // Create a new gateway for connecting to our peer node.
+        await gateway.connect(ccp, { wallet, identity: 'admin', discovery: { enabled: false } });
+
+        // Get the CA client object from the gateway for interacting with the CA.
+        const ca = gateway.getClient().getCertificateAuthority();
+        const adminIdentity = gateway.getCurrentIdentity();
+        // Register the user, enroll the user, and import the new identity into the wallet.
+        const secret = await ca.register({ affiliation: 'org1.department1', enrollmentID: username, enrollmentSecret: password, role: 'client' }, adminIdentity);
+        console.log(secret);
+        const enrollment = await ca.enroll({ enrollmentID: username, enrollmentSecret: password });
+        const userIdentity = X509WalletMixin.createIdentity('Org1MSP', enrollment.certificate, enrollment.key.toBytes());
+        wallet.import(username, userIdentity);
+        console.log('Successfully registered and enrolled admin user "' + username + '" and imported it into the wallet');
+        return true;
+
+    } catch (error) {
+        console.error(`Failed to register user ${username}: ${error}`);
+        process.exit(1);
+    }
+}
+
+var login = async function (username, password) {
+    // Checks wallet to determine if a user is registered in the HF Network
+    // Renders a new page if credentials check out
+    console.log('Logging In...');
+    try {
+        const userExists = await wallet.exists(username);
+        if (userExists) {
+            console.log('Log in');
+            let user_path = path.join('./wallet', username, 'nice.json');
+            console.log(user_path);
+            let json = require('./wallet/Danny/nice.json'); //(with path)
+            console.log(json);
+            if (password === json.enrollmentSecret) {
+                console.log('Login Successful!');
+                return true;
+            }
+            else {
+                console.log('Password is Incorrect');
+                return false;
+            }
+        }
+        else {
+            console.log('User does not exist');
+            return false;
+        }
+    } catch (error) {
+        console.error(`Failed to login user ${username}: ${error}`);
+        process.exit(1);
+    }
+}
+
+app.use('/register', function (req, res) {
+    var username = req.body.regisData.username;
+    var password = req.body.regisData.password;
+    if (register(username, password)) {
+        res.status(200).json({ message: 'OK' });
+    } else {
+        res.status(200).json({ message: 'NOK' });
+    }
+});
+
+app.use('/enroll', function (req, res) {
+    var id = req.body.adminData.id;
+    var pw = req.body.adminData.pw;
+    console.log("what is here: " + req);
+    if (enrollAdmin(id, pw)) {
+        res.status(200).json({ message: 'OK' });
+    } else {
+        res.status(200).json({ message: 'NOK' });
+    }
+});
+
+app.use('/login', function (req, res) {
+    var username = req.body.loginData.username;
+    var password = req.body.loginData.password;
+    if (login(username, password)) {
+        res.status(200).json({ message: 'OK' });
+    } else {
+        res.status(200).json({ message: 'NOK' });
+    }
+});
+
+
+app.listen(port, () => console.log(`Express Wallet app listening on port ${port}!`))
---
+++ frontend/src/components/Interface.js
@@ -0,0 +1,181 @@
+import React, { Component } from 'react';
+import $ from 'jquery';
+
+class Interface extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      login_username: "",
+      login_password: "",
+      regis_username: "",
+      regis_password: "",
+      admin_id: "",
+      admin_pw: "",
+      login_success: "",
+      regis_success: "",
+      enroll_success: ""
+    };
+
+    this.onChange = this.onChange.bind(this);
+    this.handleUserLoginChange = this.handleUserLoginChange.bind(this);
+    this.handleUserRegisChange = this.handleUserRegisChange.bind(this);
+    this.handlePasswordLoginChange = this.handlePasswordLoginChange.bind(this);
+    this.handlePasswordRegisChange = this.handlePasswordRegisChange.bind(this);
+    this.handleAdminIdChange = this.handleAdminIdChange.bind(this);
+    this.handleAdminPwChange = this.handleAdminPwChange.bind(this);
+    this.callLogin = this.callLogin.bind(this);
+    this.callEnrollAdmin = this.callEnrollAdmin.bind(this);
+    this.callRegister = this.callRegister.bind(this);
+
+  }
+
+  onChange = e => {
+    this.setState({
+      [e.target.id]: e.target.value
+    });
+  }
+
+  handleUserLoginChange = event => {
+    event.preventDefault()
+    this.setState({ login_username: event.target.value });
+  }
+
+  handlePasswordLoginChange = event => {
+    event.preventDefault()
+    this.setState({ login_password: event.target.value });
+  }
+
+  handleUserRegisChange = event => {
+    event.preventDefault()
+    this.setState({ regis_username: event.target.value });
+  }
+
+  handlePasswordRegisChange = event => {
+    event.preventDefault()
+    this.setState({ regis_password: event.target.value });
+  }
+
+  handleAdminIdChange = event => {
+    event.preventDefault()
+    this.setState({ admin_id: event.target.value });
+    console.log(this.state.admin_id);
+  }
+
+  handleAdminPwChange = event => {
+    event.preventDefault()
+    this.setState({ admin_pw: event.target.value });
+    console.log(this.state.admin_pw);
+  }
+
+  callLogin() {
+    if (this.state.login_password && this.state.login_username) {
+      const loginData = {
+        username: this.state.username,
+        password: this.state.password
+      }
+      $.ajax({
+        url: 'http://localhost:4000/login',
+        type: 'POST',
+        contentType: "application/json; charset=utf-8",
+        crossDomain: true,
+        dataType: 'json',
+        xhrFields: { withCredentials: true },
+        data: { loginData: loginData},
+        success: (data) => {
+          if (data.message === "OK") {
+            this.setState({ login_success: true });
+            console.log('success');
+          } else {
+            this.setState({ login_success: false });
+            console.log('failure');
+          }
+        }
+      });
+    } else {
+      alert("Please fill in login form fields.")
+    }
+  }
+
+  callRegister() {
+    if (this.state.regis_password && this.state.regis_username) {
+      const regisData = {
+        username: this.state.username,
+        password: this.state.password
+      }
+      $.ajax({
+        url: 'http://localhost:4000/register',
+        type: 'POST',
+        contentType: "application/json; charset=utf-8",
+        crossDomain: true,
+        dataType: 'json',
+        xhrFields: { withCredentials: true },
+        data: { regisData: regisData},
+        success: (data) => {
+          if (data.message === "OK") {
+            this.setState({ regis_success: true });
+            console.log('success');
+          } else {
+            this.setState({ regis_success: false });
+            console.log('failure');
+          }
+        }
+      });
+    } else {
+      alert("Please fill in register form fields.")
+    }
+  }
+
+  callEnrollAdmin() {
+    if (this.state.admin_id && this.state.admin_pw) {
+      const adminData = {
+        id: this.state.admin_id,
+        pw: this.state.admin_pw
+      }
+      $.ajax({
+        url: 'http://localhost:4000/enroll',
+        type: 'POST',
+        contentType: "application/json; charset=utf-8",
+        crossDomain: true,
+        dataType: 'json',
+        xhrFields: { withCredentials: true },
+        data: { adminData: adminData},
+        success: (data) => {
+          if (data.message === "OK") {
+            this.setState({ enroll_success: true });
+            console.log('success');
+          } else {
+            this.setState({ enroll_success: false });
+            console.log('failure');
+          }
+        }
+      });
+    }
+    else {
+      alert("Please fill in admin form fields.")
+    }
+  }
+
+  render() {
+    return (
+      <div className="interface">
+        <div className="loginForm">
+          <input type="text" className="loginFormField" placeholder="Username" onChange={this.handleUserLoginChange} />
+          <input type="password" className="loginFormField" placeholder="Password" onChange={this.handlePasswordLoginChange} />
+          <input type="submit" value="Login" onClick={this.callLogin}></input>
+        </div>
+        <div className="registerUserForm">
+          <input type="text" className="regFormField" placeholder="Username" onChange={this.handleUserRegisChange} />
+          <input type="password" className="regFormField" placeholder="Password" onChange={this.handlePasswordRegisChange} />
+          <input type="submit" value="Register" onClick={this.callRegister}></input>
+        </div>
+        <div className="enrollAdminForm">
+          <input type="text" className="adFormField" placeholder="Admin ID" onChange={this.handleAdminIdChange} />
+          <input type="password" className="adFormField" placeholder="Admin Password" onChange={this.handleAdminPwChange} />
+          <input type="submit" value="Enroll" onClick={this.callEnrollAdmin}></input>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Interface;
